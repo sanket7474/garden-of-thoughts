14f7af9c1c2edcf6d48737f9decf6886f5d14ea7
sanket7474 <sanketmaske10@gmail.com>
2026-03-14 01:13:59 +0530

    updated astro config for cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // needed for cloudflare pages to work properly 
+    output: "static"
+});
