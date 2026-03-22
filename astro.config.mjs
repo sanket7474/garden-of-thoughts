ca5e96bad2054f07f6ba95ed77f77f16ee0e9401
sanket7474 <sanketmaske10@gmail.com>
2026-03-22 23:59:02 +0530

    First post and minor UI fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
     // needed for cloudflare pages to work properly 
-    output: "static"
+    output: "static",
+    server: { port: 4321 },
+  integrations: [sitemap()],
+  site: 'https://sanketmaske.dev',
+  markdown: {
+    shikiConfig: {
+      theme: 'material-theme-lighter',
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          properties: {
+            className: ['external'],
+          },
+          rel: [],
+        },
+      ],
+    ],
+  },
 });
